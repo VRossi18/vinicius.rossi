9bd5ec355b0e1114bb18f5524cf16a3eeadcbfbd
Vinicius Pimenta Rossi <vinicius.rossi@zup.com.br>
2020-11-05 22:36:07 -0300

    desenvolvimento da prova

---
+++ Prova3/exe1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "TDLinkedList.h"
+
+struct DlinkedList {   
+    DLNode *begin;   
+    DLNode *end;   
+    int size; 
+}; 
+
+struct dlnode {    
+    int data;    
+    DLNode *next;    
+    DLNode *prev;
+}; 
+
+//Exercício 1a
+int list_erase_even(List* li) {
+    if (li == NULL || li->size == 0)
+        return -1;
+    else {
+        DLnode *no;
+        no = li->begin;
+        int i = 1;
+
+        while (no != NULL && i < li->size) {
+            if (no->data % 2 == 0) {
+                list_erase(li, i);
+            } else {
+                no = no->next;
+                i++;
+            }
+        }
+    }
+    return 0;
+}
+
+//Exercicio 1c
+int list_splice(List *dest, List *source, int  pos) { 
+    if (dest == NULL || source == NULL || pos > dest->size)
+        return -1;
+    else {
+        DLnode *no = dest->end;
+        for (int o = dest->size; o >= pos; o--)
+            no = no->prev;
+        
+        int i = pos;
+        DLnode aux = source->begin;
+        while (li != NULL && i > source->size) {
+            li->next = aux;
+            aux = aux->next;
+        }
+        for (int x = dest->size; x <= dest->size + source->size; x++) {
+            if (no->next != NULL) {
+                li->end->next = no->data;
+                no = no->next;
+            } else {
+                return 0;
+            }
+        }
+        return 0;
+    }
+}
---
+++ Prova3/exe2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "TStack.h" 
+ 
+struct stack {    
+    char c[MAX];
+    int size;
+};
+
+int main() {
+    char s[100];
+    if (verifica(s))
+        printf(“válida”);
+    else 
+        printf(“inválida”); 
+} 
+ 
+int verifica(char *s) {
+    char pilha[100] = s;
+    for (int o = 0; s[o] != '\0'; ++i) {
+        if (s[o] == ')') {
+            if (s[o] + 1 != '(')
+                return -1
+        }
+        if (s[0] == ']') {
+            if (s[o] + 1 != '[')
+                return -1;
+        }
+    }
+    return 0;
+}
